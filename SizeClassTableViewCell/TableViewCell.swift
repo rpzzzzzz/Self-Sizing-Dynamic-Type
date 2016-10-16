c70c068522320e7d0d92a9c7a987ba9a7abe3279
Suraphan Laokondee <suraphan.l@kaidee.com>
2016-10-16 16:13:20 +0700

    init

---
+++ SizeClassTableViewCell/TableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  TableViewCell.swift
+//  SizeClassTableViewCell
+//
+//  Created by Suraphan Laokondee on 10/15/2559 BE.
+//  Copyright © 2559 Suraphan Laokondee. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var label: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+    }
+    
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+}
--- SizeClassTableViewCell/ViewController.swift
+++ SizeClassTableViewCell/ViewController.swift
@@ -9,17 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.estimatedRowHeight = 60
+        tableView.rowHeight = UITableViewAutomaticDimension
+    }
+    
+}
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-  }
+extension ViewController: UITableViewDelegate {
+ 
+}
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
-
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! TableViewCell
+        cell.label.adjustsFontForContentSizeCategory = true
+        return cell
+    }
 }
 
